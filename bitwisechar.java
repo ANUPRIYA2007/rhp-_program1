e453a61a7ad216e8ad90f62f47b1e82d05586249
ANUPRIYA  R <anupriyarajaraman07@gmail.com>
2026-06-03 21:15:24 +0530

    Add files via upload

---
+++ bitwisechar.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class bitwisechar
+{
+    public static void main(String args[]){
+      Scanner sc=new Scanner(System.in);
+      String s = sc.nextLine();
+      int lowercase=0;
+      int uppercase=0;
+      for(char ch:s.toCharArray()){
+        if(ch>='a'&&ch<='z'){
+          lowercase |= (1<<(ch-'a'));
+        }
+        if(ch>='A'&&ch<='Z'){
+          uppercase |= (1<<(ch-'A'));
+        }
+      }
+        int all =(1<<26)-1;
+        if(lowercase == all && uppercase ==all){
+          System.out.println("All contain");
+        }
+        else{
+          System.out.println("Not contain");
+        }
+
+
+
+
+    }
+
+
+
+      }      
+     
+    
+
+
+
